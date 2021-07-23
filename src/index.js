7289ae39e142a96dd77e3feebd10edd73787d8be
Shulman <bs250204@ncr.com>
2021-07-23 13:28:19 +0300

    add patch

--- src/index.js
+++ src/index.js
@@ -55,6 +55,29 @@ app.get('/users/:id',async (req, res)=>{
     }
 })
 
+app.patch('/users/:id',async (req, res)=>{
+    const _id = req.params.id
+    const update = req.body
+
+    const updates = Object.keys(update)
+    
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid Updates!'})
+    }
+
+    try{
+        const user = await Users.findByIdAndUpdate(_id,  update, {new: true, runValidators: true})
+        if (!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }catch(error){
+        res.status(400).send(error.message)
+    }
+})
 
 app.get('/tasks', async (req, res)=>{
     try{
@@ -79,7 +102,27 @@ app.get('/tasks/:id',async (req, res)=>{
     }
 })
 
+app.patch('/tasks/:id',async (req, res)=>{
+    const _id = req.params.id
+    const update = req.body
+    const updated = Object.keys(update)
+    const allowedUpdates = ['name', 'completed']
+    const isValidOperation = updated.every((update)=>allowedUpdates.includes(update))
 
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid Updates!'})
+    }
+
+    try{
+        const tasks = await Tasks.findByIdAndUpdate(_id,  update, {new: true, runValidators: true})
+        if (!tasks) {
+            return res.status(404).send()
+        }
+        res.send(tasks)
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
