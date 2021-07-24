b32fc15bf5e80f7cea92a444a521f822b78e659a
Shulman <bs250204@ncr.com>
2021-07-24 17:01:44 +0300

    add login

--- src/models/users.js
+++ src/models/users.js
@@ -12,6 +12,7 @@ const userSchema = new mongoose.Schema({
     email:{
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase:true,
         validate:{
@@ -53,6 +54,22 @@ userSchema.pre('save', async function(next){
 
     next()
 })
+
+userSchema.statics.findByCredentials = async (email, password)=>{
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new Error('Unable to login')
+    }
+    
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch){
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
 
 const User = mongoose.model('User',userSchema)
 
