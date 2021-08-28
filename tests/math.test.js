03bde0306c5c24ae809f46f7e8e30c32ca5aa1b8
icbob99 <icbob99@gmail.com>
2021-08-28 14:51:23 +0300

    Add tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,34 @@
-test('Hello WOrld!', ()=>{
+const { TestWatcher } = require('jest')
+// const math = require('../src/math')
+const {calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add} = require('../src/math')
 
+test('Should calculate total with value', ()=>{
+    const total = calculateTip(10, .3)
+    
+    expect(total).toBe(13)
 })
 
-test('This should fail', ()=>{
-    throw new Error('Failure!')
+test('Should calculate total with default tipPercenation', ()=>{
+    const total = calculateTip(10)
+    
+    expect(total).toBe(11.2)
+})
+
+test('Should convert 32 F to 0 C', ()=>{
+    const actual = fahrenheitToCelsius(32);
+
+    expect(actual).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', ()=>{
+    const actual = celsiusToFahrenheit(0);
+
+    expect(actual).toBe(32)
+})
+
+test('Async Should add 2 nu mbers', (done)=>{
+    add(2,3).then((sum)=>{
+        expect (sum).toBe(5)
+        done()
+    })
 })
